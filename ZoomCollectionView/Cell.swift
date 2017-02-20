9d52cbaf3eb02ab1c9e523f7b953fe8831d53c18
Sergey <ithon_hunt@mail.ru>
2017-02-20 16:03:00 +0700

    First

---
+++ ZoomCollectionView/Cell.swift
@@ -0,0 +1,17 @@
+//
+//  Cell.swift
+//  GalleryTest
+//
+//  Created by Sergey on 20.02.17.
+//  Copyright © 2017 Sergey Kim. All rights reserved.
+//
+
+import UIKit
+
+final class Cell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+}
